0c5dc705383d3278f27d0367283f704034296cef
HugoWachalski <hugo.wachalski@gmail.com>
2024-02-22 14:05:36 +0100

    backend update 2.0

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,51 @@
-const express = require('express');
 
+const express = require('express');
+const path = require('path');
+const authRoutes = require('./routes/Auth.routes');
+
+/* Initialisation de l'API */
 const app = express();
+
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
     next();
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
-});
+
+/* Securite en tete */
+const helmet = require("helmet");
+
+app.use(helmet());
+
+/* RateLimit */
+const rateLimit = require("express-rate-limit");
+
+app.use(
+    rateLimit({
+        windowMs: 10 * 60 * 1000,
+        max: 100,
+        message:
+            "Vous avez effectué plus de 100 requêtes dans une limite de 10 minutes!",
+        headers: true,
+    })
+);
+
+/* Mise en place du routage */
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
