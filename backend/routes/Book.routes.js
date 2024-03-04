ce9d744d9c104be68aac15e011a63aec792ec231
HugoWachalski <hugo.wachalski@gmail.com>
2024-03-04 09:31:57 +0100

    New routes added for delete and put

--- backend/routes/Book.routes.js
+++ backend/routes/Book.routes.js
@@ -12,8 +12,8 @@ router.get('/:id', BookCtrl.getBookById);
 //router.get('/bestrating', BookCtrl.getBestBooks);
 router.post('/', GuardAuth, GuardMulter, BookCtrl.createNewBook);
 //router.post('/:id/rating', GuardAuth, BookCtrl.addNewGrade);
-//router.put('/:id', GuardAuth, BookCtrl.updateBook);
-//router.delete('/:id', GuardAuth, BookCtrl.deleteBook);
+router.put('/:id', GuardAuth, BookCtrl.updateBook);
+router.delete('/:id', GuardAuth, BookCtrl.deleteBook);
 
 
 module.exports = router;
